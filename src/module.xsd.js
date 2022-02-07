18b4d09ae976568abce0a09ca59f0e103c18cd4e
Simon Petrac <pfoerdie@gmail.com>
2022-02-07 14:19:40 +0100

    initial commit

---
+++ src/module.xsd.js
@@ -0,0 +1,6 @@
+const
+    util    = require('@nrd/fua.core.util'),
+    assert  = util.Assert('module.xsd'),
+    context = require('@nrd/fua.resource.data/context');
+
+// TODO
---
+++ test/module.xsd.test.js
@@ -0,0 +1,10 @@
+const
+    {describe, test} = require('mocha'),
+    expect           = require('expect'),
+    xsd              = require('../src/module.xsd.js');
+
+describe('module.xsd', function () {
+
+    // TODo
+
+});
